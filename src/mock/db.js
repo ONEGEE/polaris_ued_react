37aaa0b0bf1a1e3dd3d1c76a5926dc5a5f1d521a
ONEGEE <onegee16@gmail.com>
2018-07-08 05:45:25 +0800

    config json-server routes

--- src/mock/db.js
+++ src/mock/db.js
@@ -1,9 +1,51 @@
 module.exports = () => {
 	const db = {
-		posts: [
+		docsList: [
 			{
 				id: 1,
-				title: 'posts'
+				guid: 1,
+				title: 'Intro'
+			},
+			{
+				id: 2,
+				guid: 2,
+				title: 'Team Intro'
+			},
+			{
+				id: 3,
+				guid: 3,
+				title: 'Standard'
+			},
+			{
+				id: 4,
+				guid: 4,
+				title: 'Log'
+			}
+		],
+		docsDetails: [
+			{
+				id: 1,
+				guid: 1,
+				title: 'Intro',
+				desc: 'Intro this project.'
+			},
+			{
+				id: 2,
+				guid: 2,
+				title: 'Team',
+				desc: 'Team introduction.'
+			},
+			{
+				id: 3,
+				guid: 3,
+				title: 'Standard',
+				desc: 'Standard docs.'
+			},
+			{
+				id: 4,
+				guid: 4,
+				title: 'Log',
+				desc: 'Log by versions.'
 			}
 		]
 	}
