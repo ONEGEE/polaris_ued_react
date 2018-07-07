4f542e20e2e19af536717a527d5573c5e6dad750
ONEGEE <onegee16@gmail.com>
2018-07-08 05:46:29 +0800

    styled Footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 const FooterWrapper = styled.div`
-	background: #aaa;
-	color: #333;
+	color: #999;
 	text-align: center;
+	font-size: 12px;
 `
 
 const Footer = () => {
-	return <FooterWrapper>Footer</FooterWrapper>
+	return <FooterWrapper>Power by ONEGEE</FooterWrapper>
 }
 
 export default Footer
