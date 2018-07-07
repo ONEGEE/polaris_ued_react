ade913a40807d4863b47074e5b344bf076d81f1e
ONEGEE <onegee16@gmail.com>
2018-07-08 05:49:39 +0800

    modify query error message

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,6 +1,6 @@
 export const query = (path, options) => {
 	if (!path.match(/^\//)) {
-		throw Error('every fetch path must be start with "/"')
+		throw Error('Every fetch path must be start with "/"')
 	} else {
 		let url = `http://localhost:8888/${path.slice(1)}`
 		return fetch(url, options)
