3fd357a23760e109c8fbb68a72611cfafcc30352
ONEGEE <onegee16@gmail.com>
2018-07-08 05:47:23 +0800

    styled Layout

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import styled from 'styled-components'
+import { Container } from 'semantic-ui-react'
 
 const StyledHeader = styled(Header)`
   flex: 0;
 `
-const StyledMain = styled.div`flex: 1;`
+const StyledMain = styled.div`
+	flex: 1;
+	margin-top: 25px;
+`
 const StyledFooter = styled(Footer)`
   flex: 0;
 `
@@ -15,7 +19,9 @@ export class Layout extends Component {
 		return (
 			<React.Fragment>
 				<StyledHeader />
-				<StyledMain>{this.props.children}</StyledMain>
+				<StyledMain>
+					<Container>{this.props.children}</Container>
+				</StyledMain>
 				<StyledFooter />
 			</React.Fragment>
 		)
