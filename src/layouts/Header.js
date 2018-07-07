1e6f8d5d7f6d54c3b35986d05765a976e7a82e70
ONEGEE <onegee16@gmail.com>
2018-07-08 05:47:05 +0800

    rewrite Header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,30 +1,64 @@
 import React from 'react'
-import styled from 'styled-components'
-import { NavLink } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+import { Segment, Container, Menu, Header as Title } from 'semantic-ui-react'
 
-const HeaderWraper = styled.div`
-	background: #000;
-	color: #fff;
-	height: 50px;
-	width: 100%;
-	text-align: center;
-`
+class Header extends React.Component {
+	handleItemClick = (path) => {
+		this.props.history.push(path)
+	}
+	render () {
+		const { location } = this.props
+		let navList = [
+			{
+				name: 'home',
+				path: '/home/'
+			},
+			{
+				name: 'readme',
+				path: '/readme/intro'
+			},
+			{
+				name: 'dev',
+				path: '/dev/'
+			},
+			{
+				name: 'library',
+				path: '/library/'
+			},
+			{
+				name: 'resources',
+				path: '/resources/'
+			}
+		]
 
-const Header = () => {
-	return (
-		<HeaderWraper>
-			<ul>
-				<li>
-					<NavLink to='/'>Polaris</NavLink>
-					<NavLink to='/'>Home</NavLink>
-					<NavLink to='/readme'>ReadMe</NavLink>
-					<NavLink to='/dev'>Dev</NavLink>
-					<NavLink to='/library'>Library</NavLink>
-					<NavLink to='/resources'>Resources</NavLink>
-				</li>
-			</ul>
-		</HeaderWraper>
-	)
+		const makeMenuItem = navList.map((nav, index) => {
+			let reg = new RegExp(`^/${nav.name}`)
+			return (
+				<Menu.Item
+					name={nav.name}
+					active={reg.test(location.pathname)}
+					as='a'
+					onClick={() => this.handleItemClick(nav.path)}
+					key={index}
+				/>
+			)
+		})
+
+		return (
+			<Segment inverted vertical>
+				<Container>
+					<Menu inverted pointing secondary>
+						<Menu.Item>
+							<Link to='/home/'>
+								<Title inverted>Polaris</Title>
+							</Link>
+						</Menu.Item>
+						<Menu.Menu position='right'>{makeMenuItem}</Menu.Menu>
+					</Menu>
+				</Container>
+			</Segment>
+		)
+	}
 }
 
-export default Header
+export default withRouter(Header)
