b7d1ed49d52578e82eab2f2aaccc9034eef172f3
ONEGEE <onegee16@gmail.com>
2018-07-10 23:14:39 +0800

    fix query bug

--- src/utils/query.js
+++ src/utils/query.js
@@ -2,7 +2,7 @@ export const query = (path, options) => {
 	if (!path.match(/^\//)) {
 		throw Error('Every fetch path must be start with "/"')
 	} else {
-		let url = `http://localhost:8888/${path.slice(1)}`
+		let url = `http://localhost:8888${path}`
 		return fetch(url, options)
 	}
 }
