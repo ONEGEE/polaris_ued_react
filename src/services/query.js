1ed12f49fa9fef9675cd22b8d4515d6f7eaafa88
ONEGEE <onegee@ONEGEEdeMBP.lan>
2018-07-20 18:00:11 +0800

    :wrench: build services and move query

---
+++ src/services/query.js
@@ -0,0 +1,14 @@
+export const query = (path, options) => {
+	if (!path.match(/^\//)) {
+		throw Error('Every fetch path must be start with "/"')
+	} else {
+		let url = `http://localhost:8888${path}`
+		return fetch(url, options).then((res) => res.json())
+	}
+}
+
+export const getDocsList = () => query('/docs/list')
+
+export const getActiveDocByGuid = (guid) => query(`/docs/details/${guid}`)
+
+export const getLibraryTOC = () => query('/libraryTOC')
--- src/utils/query.js
+++
@@ -1,8 +0,0 @@
-export const query = (path, options) => {
-	if (!path.match(/^\//)) {
-		throw Error('Every fetch path must be start with "/"')
-	} else {
-		let url = `http://localhost:8888${path}`
-		return fetch(url, options)
-	}
-}
