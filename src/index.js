684935e78955010042f21d54a13bd238bac49250
ONEGEE <onegee@ONEGEEdeMBP.lan>
2018-07-20 18:36:52 +0800

    :heavy_plus_sign: add semantic-ui-css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@/index.css'
+import 'semantic-ui-css/semantic.min.css'
 
 import ReactRouter from '@/routes'
 import registerServiceWorker from './utils/registerServiceWorker'
